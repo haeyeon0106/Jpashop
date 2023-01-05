a7a1c484105762071c6c3cd72b4688ff841b8f46
haeyeon0106 <yeon99016@gmail.com>
2023-01-06 01:41:34 +0900

    MemberRepository / MemberService / Test

---
+++ src/test/java/jpabook/jpashop/service/MemberServiceTest.java
@@ -0,0 +1,59 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.repository.MemberRepository;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+@SpringBootTest
+@Transactional  // 기본적으로 Rollback
+public class MemberServiceTest {
+
+    @Autowired MemberService memberService;
+    @Autowired MemberRepository memberRepository;
+    @Autowired EntityManager em;
+
+    @Test
+    public void 회원가입() throws Exception{
+        // given
+        Member member = new Member();
+        member.setName("kim");
+
+        // when
+        Long saveId = memberService.join(member);
+
+        // then
+        //em.flush();     // DB에 값 저장(강제로 DB에 쿼리가 나감)
+        assertEquals(member,memberService.findOne(saveId));
+    }
+
+    @Test(expected = IllegalStateException.class)   // 예외잡기(try ~ catch 대신 사용)
+    public void 중복_회원_예외() throws Exception{
+        // given
+        Member member1 = new Member();
+        member1.setName("kim");
+
+        Member member2 = new Member();
+        member2.setName("kim");
+
+        // when
+//        memberService.join(member1);
+//        try{
+//            memberService.join(member2);    // 예외 발생
+//        }catch (IllegalStateException e){
+//            return;
+//        }
+
+        memberService.join(member1);
+        memberService.join(member2);
+        // then
+        fail("예외발생");
+    }
+}
